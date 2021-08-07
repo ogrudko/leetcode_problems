c332c242d868eea72db6b1f5e091c9ffc21e05f7
ogrudko <ogrudko@omilia.com>
2021-08-07 21:41:52 +0300

    refactor valid_parentheses and create tests for it

--- easy/valid_parentheses.py
+++ easy/valid_parentheses.py
@@ -11,23 +11,23 @@ Open brackets must be closed in the correct order.
 
 # Solution
 
-user_string = input("Input your parentheses:\n")
-left_brackets = ["(", "[", "{"]
-right_brackets = [")", "]", "}"]
-stack = []
-correct_parentheses = True
-for char in user_string:
-    if char in left_brackets:
-        stack.append(char)
-    elif char in right_brackets:
-        if len(stack) <= 0:
-            correct_parentheses = False
-            break
-        if left_brackets.index(stack.pop()) != right_brackets.index(char):
-            correct_parentheses = False
-            break
-if len(stack) != 0:
-    correct_parentheses =  False
-
-print(correct_parentheses)
+class Solution():
+    def is_valid_parentheses(self, user_string: str) -> bool:
+        left_brackets = ["(", "[", "{"]
+        right_brackets = [")", "]", "}"]
+        stack = []
+        is_valid = True
+        for char in user_string:
+            if char in left_brackets:
+                stack.append(char)
+            elif char in right_brackets:
+                if len(stack) <= 0:
+                    is_valid = False
+                    break
+                if left_brackets.index(stack.pop()) != right_brackets.index(char):
+                    is_valid = False
+                    break
+        if len(stack) != 0:
+            is_valid = False
+        return is_valid
 
